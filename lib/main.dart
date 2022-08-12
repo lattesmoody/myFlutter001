f1786cc5214881d67467bf8c98d36bb01273e0f6
lattesmoody <lattesmoody@gmail.com>
2022-08-12 14:47:09 +0900

    Class_8

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,25 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'First app',
       theme: ThemeData(primarySwatch: Colors.blue),
-      home: MyHomePage(),
+      home: MyHomePage(), // 앱 실행 시 제일 먼저 보여지는 경로.
+    );
+  }
+}
+
+class MyHomePage extends StatelessWidget {
+  const MyHomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('First App'),
+      ),
+      body: Center(
+        child: Column(
+          children: [Text('Hello'), Text('Hello'), Text('Hello')],
+        ),
+      ),
     );
   }
 }
