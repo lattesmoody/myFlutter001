fc8cb289420e88e3220b53c98fa61442e06c2fe7
lattesmoody <lattesmoody@gmail.com>
2022-08-12 14:36:41 +0900

    Class_7

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,16 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp())
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'First app',
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyHomePage(),
+    );
+  }
+}
