aab52c067b9accdc1216ddaebdae1f3607d5448b
lattesmoody <lattesmoody@gmail.com>
2022-09-13 14:22:21 +0900

    class_26_new version code

--- lib/main.dart
+++ lib/main.dart
@@ -9,8 +9,40 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Snack Bar',
-      theme: ThemeData(primarySwatch: Colors.red),
-      home: HomeBody(),
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: MyPage(),
+    );
+  }
+}
+
+class MyPage extends StatelessWidget {
+  const MyPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Scaffold Messenger'),
+      ),
+      body: HomeBody(),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.thumb_up),
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+                content: Text('Like a new Snack Bar!'),
+                duration: Duration(seconds: 5),
+                action: SnackBarAction(
+                    label: 'Undo',
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(builder: (context) => ThirdPage()),
+                      );
+                    })),
+          );
+        },
+      ),
     );
   }
 }
@@ -46,6 +78,36 @@ class SecondPage extends StatelessWidget {
       body: const Center(
         child:
             Text("안녕하세요", style: TextStyle(fontSize: 20.0, color: Colors.red)),
+      ),
+    );
+  }
+}
+
+class ThirdPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Second Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('"좋아요"를 취소 하시겠습니까?',
+                style: TextStyle(fontSize: 20.0, color: Colors.red)),
+            ElevatedButton(
+                onPressed: () {
+                  ScaffoldMessenger.of(context).showSnackBar(
+                    SnackBar(
+                      content: Text('"좋아요"가 취소되었습니다'),
+                      duration: Duration(seconds: 3),
+                    ),
+                  );
+                },
+                child: Text('취소하기'))
+          ],
+        ),
       ),
     );
   }
